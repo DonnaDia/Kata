17679769f7627cf36b52843f62ff831e3b725111
Donna Dia <0dvd0bvb0@gmail.com>
2018-12-24 19:24:47 +0200

    'amends'

--- Python/8kyu/Count Odd Numbers below n.py
+++ Python/8kyu/Count Odd Numbers below n.py
@@ -8,3 +8,18 @@ def odd_count(n):
     return len(nums)
 
 print(odd_count(15023))
+
+
+#Vers2
+def odd_count(n):
+    nums = []
+    for number in range(1, n, 2):
+         nums.append(number)
+
+    return len(nums)
+
+print(odd_count(15023))
+
+#Vers3
+def odd_count(n):
+    return n // 2
