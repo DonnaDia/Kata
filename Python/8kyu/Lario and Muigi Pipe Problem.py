975b128a707032bc5ff53751aa149bee94131a59
Donna Dia <0dvd0bvb0@gmail.com>
2019-04-10 15:28:51 +0300

    '10.04.2019'

---
+++ Python/8kyu/Lario and Muigi Pipe Problem.py
@@ -0,0 +1,8 @@
+#Vers 1
+def pipe_fix(input):
+	return [num for num in range(input[0], input[-1] + 1)]
+
+#Vers 2
+def pipe_fix(input):
+    output = [num for num in range(input[0], input[-1] + 1)]
+    return output
